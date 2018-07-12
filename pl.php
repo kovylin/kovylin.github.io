955139a2ba23a56420c6ae48a7e222d9921f7af0
kovylin77 <kovylinn77@gmail.com>
2018-07-13 00:32:10 +0300

    added translations

---
+++ pl.php
@@ -0,0 +1,441 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>MARKA LOGISTIC - Road reapair services in Poland</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="icon" href="images/favicon.png">
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+                <!-- Inner -->
+                    <div class="inner">
+                        <header>
+                            <h1><a href="index.html" id="logo">MARKA LOGISTIC</a></h1>
+                            <hr />
+                            <p>Road repair &amp; related services in Poland</p>
+                        </header>
+                        <footer>
+                            <a href="#footer" class="button circled scrolly">CONTACT</a>
+                        </footer>
+                    </div>
+				</div>
+
+			<!-- Banner -->
+				<section id="banner">
+					<header>
+						<h2>If you're looking for a high-quality <strong>road repair</strong> services <br>then you got in the right place.</h2>
+						<p>
+                            Our company will help you with resolution of any related problem.<br>
+                            <b>What we offer:</b>
+						</p>
+					</header>
+				</section>
+
+			<!-- Carousel -->
+<!--
+				<section class="carousel">
+					<div class="reel">
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Pulvinar sagittis congue</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Fermentum sagittis proin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Ultrices urna sit lobortis</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Varius magnis sollicitudin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Pulvinar sagittis congue</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Fermentum sagittis proin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Ultrices urna sit lobortis</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+							<header>
+								<h3><a href="#">Varius magnis sollicitudin</a></h3>
+							</header>
+							<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+						</article>
+
+					</div>
+				</section>
+-->
+
+			<!-- Main -->
+<!--
+				<div class="wrapper style2">
+
+					<article id="main" class="container special" style=" text-align: center">
+						<a href="#" class="image featured"><img style="max-height: 500px; max-width: 800px; display: inline-block;" src="images/machine1.jpg" alt="" /></a>
+						<header>
+							<h2><a href="#">Sed massa imperdiet magnis</a></h2>
+							<p>
+								Sociis aenean eu aenean mollis mollis facilisis primis ornare penatibus aenean. Cursus ac enim
+								pulvinar curabitur morbi convallis. Lectus malesuada sed fermentum dolore amet.
+							</p>
+						</header>
+						<p>
+							Commodo id natoque malesuada sollicitudin elit suscipit. Curae suspendisse mauris posuere accumsan massa
+							posuere lacus convallis tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus
+							sit arcu sociis. Nunc fermentum adipiscing tempor cursus nascetur adipiscing adipiscing. Primis aliquam
+							mus lacinia lobortis phasellus suscipit. Fermentum lobortis non tristique ante proin sociis accumsan
+							lobortis. Auctor etiam porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum
+							consequat integer interdum integer purus sapien. Nibh eleifend nulla nascetur pharetra commodo mi augue
+							interdum tellus. Ornare cursus augue feugiat sodales velit lorem. Semper elementum ullamcorper lacinia
+							natoque aenean scelerisque.
+						</p>
+						<footer>
+							<a href="#" class="button">Continue Reading</a>
+						</footer>
+					</article>
+
+				</div>
+-->
+
+			<!-- Features -->
+				<div class="wrapper style1">
+
+					<section id="features" class="container special">
+<!--
+						<header style="margin-bottom: 30px;">
+							<h2>What we offer:</h2>
+							<p>Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.</p>
+						</header>
+-->
+						<div class="row features-padding">
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/w1.jpg" alt="" style="max-height: 250px; max-width: 400px" /></a>
+								<header>
+									<h3><a href="#">Residence Card</a></h3>
+								</header>
+								<p>
+									The residence card (residence permits) is a permission for a foreigner to legally reside on the territory of Poland and the Schengen zone, without the need to have any other documents. It is not always the residence card (residence permit) that gives permission to work legally, but it is always legal to stay throughout the validity period of the card.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/as5.jpg" alt="" style="height: 250px; max-width: 400px" /></a>
+								<header>
+									<h3><a href="#">Preparation Of Documents</a></h3>
+								</header>
+								<p>							
+Lawyers of Ainprard Poland have 10 years of experience in matters related to Polish law. They specialize in issues related to legalization and employment in Poland, they know everything about foreigners in Poland and are ready to help you solve all your legal problems.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/w3.jpg" alt="" style="max-height: 250px; max-width: 400px" /></a>
+								<header>
+									<h3><a href="#">VISA</a></h3>
+								</header>
+								<p>
+									If you have Polish friends or foreign friends who live in Poland for more than five years, you have the opportunity to receive a guest invitation to visit Poland. The duration of such an invitation can be from a week to a year. The main requirement is to confirm the inviting party's ability to ensure your stay in Poland for the entire duration of the validity of the guest visa. 
+								</p>
+							</article>
+						</div>
+						<div class="row">
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/as1.jpg" alt="" style="max-height: 250px; max-width: 400px" /></a>
+								<header>
+									<h3><a href="#">Work permit</a></h3>
+								</header>
+								<p>
+									According to Polish law every foreigner willing to work in Poland must legalize their employment. One of the way to do this is to obtain work permit (Zezwolenie na pracę). It is the employer entrusting the foreigner with the execution of work that applies for a work permit for the foreigner. The issuing authority is Voivode competent for the seat/ place of residence of the employer.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/w2.jpg" alt="" style="max-height: 250px; max-width: 400px" /></a>
+								<header>
+									<h3><a href="#">Citizenship of Poland</a></h3>
+								</header>
+								<p>
+									Polish citizenship can be granted to any foreigner who submitted an application to the President of the Republic of Poland. In case it is not important on the basis of which you reside in the territory of Poland, or if you have a source of stable and regular income or a place of residence.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/as2.jpg" alt="" style="max-height: 250px; max-width: 400px" /></a>
+								<header>
+									<h3><a href="#">Statement on the Intention of Entrusting Work to a Foreigner</a></h3>
+								</header>
+								<p>
+									Citizens of Armenia, Belarus, the Russian Federation, Georgia, the Republic of Moldova and
+Ukraine may work on the basis of registered statement during the term of maximum 6 in the
+following 12 months.
+								</p>
+							</article>
+						</div>
+					</section>
+
+				</div>
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+					<p style="text-align: center; font-size: 24px; color: white;">
+					    Contacts: <br> Phone: +77777777 <br>Email: test@contact.pl
+					</p>
+<!--						<div class="row">-->
+
+							<!-- Tweets -->
+<!--
+								<section class="col-4 col-12-mobile">
+									<header>
+										<h2 class="icon fa-twitter circled"><span class="label">Tweets</span></h2>
+									</header>
+									<ul class="divided">
+										<li>
+											<article class="tweet">
+												Amet nullam fringilla nibh nulla convallis tique ante sociis accumsan.
+												<span class="timestamp">5 minutes ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+												Hendrerit rutrum quisque.
+												<span class="timestamp">30 minutes ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+												Curabitur donec nulla massa laoreet nibh. Lorem praesent montes.
+												<span class="timestamp">3 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+												Lacus natoque cras rhoncus curae dignissim ultricies. Convallis orci aliquet.
+												<span class="timestamp">5 hours ago</span>
+											</article>
+										</li>
+									</ul>
+								</section>
+-->
+
+							<!-- Posts -->
+<!--
+								<section class="col-4 col-12-mobile">
+									<header>
+										<h2 class="icon fa-file circled"><span class="label">Posts</span></h2>
+									</header>
+									<ul class="divided">
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Nisl fermentum integer</a></h3>
+												</header>
+												<span class="timestamp">3 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Phasellus portitor lorem</a></h3>
+												</header>
+												<span class="timestamp">6 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Magna tempus consequat</a></h3>
+												</header>
+												<span class="timestamp">Yesterday</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Feugiat lorem ipsum</a></h3>
+												</header>
+												<span class="timestamp">2 days ago</span>
+											</article>
+										</li>
+									</ul>
+								</section>
+-->
+
+							<!-- Photos -->
+<!--
+								<section class="col-4 col-12-mobile">
+									<header>
+										<h2 class="icon fa-camera circled"><span class="label">Photos</span></h2>
+									</header>
+									<div class="row gtr-25">
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic10.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic11.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic12.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic13.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic14.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic15.jpg" alt="" /></a>
+										</div>
+									</div>
+								</section>
+
+						</div>
+-->
+						<hr />
+						<div class="row">
+							<div class="col-12">
+
+								<!-- Contact -->
+<!--
+									<section class="contact">
+										<header>
+											<h3>Nisl turpis nascetur interdum?</h3>
+										</header>
+										<p>Urna nisl non quis interdum mus ornare ridiculus egestas ridiculus lobortis vivamus tempor aliquet.</p>
+										<ul class="icons">
+											<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+											<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+											<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+											<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+											<li><a href="#" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+										</ul>
+									</section>
+-->
+
+								<!-- Copyright -->
+								<form name="contactform" method="post" action="send_form_email.php" style="color: white;">
+<table width="450px">
+<tr style="margin-bottom: 15px;">
+ <td valign="top">
+  <label for="first_name" style="color: white;">First Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="first_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr style="margin-bottom: 15px;">
+ <td valign="top">
+  <label for="last_name" style="color: white;">Last Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="last_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr style="margin-bottom: 15px;">
+ <td valign="top">
+  <label for="email" style="color: white;">Email Address *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="email" maxlength="80" size="30">
+ </td>
+</tr>
+<tr style="margin-bottom: 30px;">
+ <td valign="top">
+  <label for="telephone" style="color: white;">Telephone Number</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="telephone" maxlength="30" size="30">
+ </td>
+</tr>
+<tr>
+<br>
+ <td colspan="2" style="text-align:center">
+ <br>
+  <input type="submit" value="Submit">
+ </td>
+</tr>
+</table>
+</form>
+									<div class="copyright">
+										<ul class="menu">
+											<li>&copy; Marka Logistic, 2018</li>
+										</ul>
+									</div>
+
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
